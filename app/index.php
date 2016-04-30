8289c55b833721de2aa174a4343c65a3a142e646
Thom <tallen.thom@gmail.com>
2016-04-30 13:48:08 -0600

    Initial commit for my site

---
+++ app/index.php
@@ -0,0 +1,6 @@
+<?php
+	// my actual file is in a different place
+	require_once("../core/homepage.php");
+?>
+
+
---
+++ core/homepage.php
@@ -0,0 +1,41 @@
+<?php
+
+	// if(!isset($_COOKIE["cookie_visits"])) {
+	// 	$cookie_num = 1;
+	// } else {
+	// 	$cookie_num = $_COOKIE["cookie_visits"];
+	// 	$cookie_num += 1;
+	// }
+
+	// setcookie("cookie_visits", $cookie_num);
+
+	// echo $_COOKIE["cookie_visits"];
+
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+	<title>.::Thom's Muffins::.</title>
+
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	    <a class="navbar-brand" href="#">
+        	<img class="gingy_img" alt="Brand" src="images/Gingy_1.jpg">
+        </a>
+		<ul class="nav navbar-nav">
+	        <li class="active"><a href="#">Thom's Muffins<span class="sr-only">(current)</span></a></li>
+	        <li class="active"><a href="#">About Me<span class="sr-only">(current)</span></a></li>
+	        <li class="active"><a href="#">Assignments<span class="sr-only">(current)</span></a></li>
+	    </ul>
+	</nav>
+
+
+
+</body>
+</html>
