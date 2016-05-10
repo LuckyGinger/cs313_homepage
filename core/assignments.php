a7fa823c63b1bafcef8f72d2a4560180a0c4f928
Thom <tallen.thom@gmail.com>
2016-05-10 10:32:35 -0600

    Making edits to my survey as well as pushing my survey to git.

--- core/assignments.php
+++ core/assignments.php
@@ -3,7 +3,7 @@
 
 <html>
 <head>
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
@@ -17,10 +17,10 @@
 	<div class="row">
 		<div class="col-md-1 col-sm-2"></div>
 		<div class="content_div col-md-7 col-sm-8">
-			<h1 class="coming_soon" >Coming Soon!</h1>
+			<h1 class="coming_soon" >Assignments:</h1>
 
-
-			<img src="./images/t_rex.gif" class="img-responsive center-block" alt="t-rex">
+			<button type="button" class="btn btn-primary btn-lg btn-block" onclick="parent.location='../app/assignments/phpSurvey/survey.php'">1. PHP Survey</button>
+			<!-- <img src="./images/t_rex.gif" class="img-responsive center-block" alt="t-rex"> -->
 
 
 		</div>
